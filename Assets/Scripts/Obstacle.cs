26c38e73f22135bdf48fa368cbf3b2cd5a5b623a
TurgayOzdemir <ozdemir.turgay@outlook.com.tr>
2022-06-25 11:14:03 +0300

    Added AI Players.

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Deðdi");
+            
+            SceneManager.LoadScene("SampleScene");
         }
+
+        else
+        {
+            other.gameObject.transform.position = new Vector3(0,0,0);
+        }
+        
     }
 }
